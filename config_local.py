cbf6e6791363feee2b26b98d8cd0e91d651a03ca
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 17:43:28 +0000

    Create config_local.py

---
+++ config_local.py
@@ -0,0 +1,9 @@
+import os
+DATA_DIR = os.path.realpath(os.path.expanduser(u'~/.pgadmin/'))
+LOG_FILE = os.path.join(DATA_DIR, 'pgadmin4.log')
+SQLITE_PATH = os.path.join(DATA_DIR, 'pgadmin4.db')
+SESSION_DB_PATH = os.path.join(DATA_DIR, 'sessions')
+STORAGE_DIR = os.path.join(DATA_DIR, 'storage')
+SERVER_MODE = False
+DEFAULT_SERVER = '0.0.0.0'
+DEFAULT_SERVER_PORT = 5050
