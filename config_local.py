566e33824eaaa19045df2ef8e2127f5440c6190c
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 18:56:36 +0000

    Does anyhost work better?

--- config_local.py
+++ config_local.py
@@ -6,5 +6,5 @@ SQLITE_PATH = os.path.join(DATA_DIR, 'pgadmin4.db')
 SESSION_DB_PATH = os.path.join(DATA_DIR, 'sessions')
 STORAGE_DIR = os.path.join(DATA_DIR, 'storage')
 SERVER_MODE = False
-#DEFAULT_SERVER = '127.0.0.1'
+DEFAULT_SERVER = '0.0.0.0'
 #DEFAULT_SERVER_PORT = 5050
