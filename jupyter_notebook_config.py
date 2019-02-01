a1225712b868b8600df9394f06177dbf6f12b13d
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 20:43:41 +0000

    No need to proxify the postgres server

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,16 +1,17 @@
 # Traitlet configuration file for jupyter-notebook.
 
 c.ServerProxy.servers = {
-    'postgres': {
-        'command': ['pg_ctl', '-D', '"${PGDATA:-/home/jovyan/srv/pgsql}"','-l','"${PGDATA:-/home/jovyan/srv/pgsql}/pg.log"','restart'],
-        'timeout': 120,
-        'launcher_entry': {
-            #We're autorunning postgres as a headless service
-            'enabled': False,
-            #'icon_path': '/home/jovyan/.jupyter/postgresql-logo.svg',
-            #'title': 'PostgreSQL',
-        },
-    },
+    # We're autostarting the postgres server, and using a hardwired, unexposed port, so we don't need to declare this
+    #'postgres': {
+    #    'command': ['pg_ctl', '-D', '"${PGDATA:-/home/jovyan/srv/pgsql}"','-l','"${PGDATA:-/home/jovyan/srv/pgsql}/pg.log"','restart'],
+    #    'timeout': 120,
+    #    'launcher_entry': {
+    #        #We're autorunning postgres as a headless service
+    #        'enabled': False,
+    #        #'icon_path': '/home/jovyan/.jupyter/postgresql-logo.svg',
+    #        #'title': 'PostgreSQL',
+    #    },
+    #},
     'pgadmin': {
         #Set the port for pgadmin dynamically
         'command': ['PGADMIN_PORT={port}','python', '/srv/conda/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py'],
