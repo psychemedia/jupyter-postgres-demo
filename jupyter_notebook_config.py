fd1827b6511f2c081d3db61e801bed239e276613
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 20:04:56 +0000

    What does a False launcher entry do???

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -5,9 +5,9 @@ c.ServerProxy.servers = {
         'command': ['pg_ctl', '-D', '"${PGDATA:-/home/jovyan/srv/pgsql}"','-l','"${PGDATA:-/home/jovyan/srv/pgsql}/pg.log"','restart'],
         'timeout': 120,
         'launcher_entry': {
-            'enabled': True,
-            'icon_path': '/home/jovyan/.jupyter/postgresql-logo.svg',
-            'title': 'PostgreSQL',
+            'enabled': False,
+            #'icon_path': '/home/jovyan/.jupyter/postgresql-logo.svg',
+            #'title': 'PostgreSQL',
         },
     },
     'pgadmin': {
