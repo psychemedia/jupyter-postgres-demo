51397e27b83f4b5fd56839bd439150b90378cbfb
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 20:41:52 +0000

    Set pgadmin port dynamically

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -5,15 +5,17 @@ c.ServerProxy.servers = {
         'command': ['pg_ctl', '-D', '"${PGDATA:-/home/jovyan/srv/pgsql}"','-l','"${PGDATA:-/home/jovyan/srv/pgsql}/pg.log"','restart'],
         'timeout': 120,
         'launcher_entry': {
+            #We're autorunning postgres as a headless service
             'enabled': False,
             #'icon_path': '/home/jovyan/.jupyter/postgresql-logo.svg',
             #'title': 'PostgreSQL',
         },
     },
     'pgadmin': {
-        'command': ['python', '/srv/conda/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py'],
+        #Set the port for pgadmin dynamically
+        'command': ['PGADMIN_PORT={port}','python', '/srv/conda/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py'],
         'timeout': 120,
-        'port': 5050,
+        #'port': 5050,
         'launcher_entry': {
             'enabled': True,
             'icon_path': '/home/jovyan/.jupyter/postgresql-logo.svg',
