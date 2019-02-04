2b2d74197c3c7a1802612d821a468d32a2c467f2
Tony Hirst <tony.hirst@gmail.com>
2019-02-04 14:18:37 +0000

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -6,6 +6,6 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Demo PostgreSQL Database initialisation
 psql postgres -c "CREATE USER test PASSWORD 'testpass'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
-createdb -O test test
+createdb -O test testdb
 
-psql -d test -U test -f $THISDIR/seed_db.sql
+psql -d testdb -U test -f $THISDIR/seed_db.sql
